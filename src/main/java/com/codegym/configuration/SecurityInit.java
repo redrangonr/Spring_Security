3b15e526889b6273c2e6622449bc5af8e752ed69
Quocanh <phiquocanh717@gmail.com>
2021-08-02 15:34:37 +0700

    security

---
+++ src/main/java/com/codegym/configuration/SecurityInit.java
@@ -0,0 +1,7 @@
+package com.codegym.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
